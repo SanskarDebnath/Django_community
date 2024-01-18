0f87625f73241219321291a6eee94b765eb3f864
sanskar0381 <sanskardebnath2019@gmail.com>
2024-01-18 10:36:00 +0530

    Update models.py

--- project_3245/web_app/models.py
+++ project_3245/web_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class signup (models.Model):
+    fullname = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    Gender = models.IntegerField(max_length=1)
+    Email = models.CharField(max_length=50,unique = True)
+    new_password = models.CharField(max_length=200)
+    user_id = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    Reg_date = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.fullname + "--" + self.user_id + "--" + self.username

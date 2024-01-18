a451758efa1694307022b2410a20047599f9ebd9
sanskar0381 <sanskardebnath2019@gmail.com>
2024-01-18 11:05:44 +0530

    gtr

--- project_3245/web_app/admin.py
+++ project_3245/web_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from web_app.models import signup
+
+
+class Signup_admin (admin.ModelAdmin):
+    list_display = (
+        'fullname', 
+        'phone_number', 
+        'Gender',
+        'Email', 
+        'user_id', 
+        'username', 
+        'Reg_date'
+        )
+
+adminl.site.register(signup, Signup_admin)
 
 # Register your models here.
